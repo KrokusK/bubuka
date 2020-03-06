822e4fa9533883991619293e2dc40ae5c573f581
KrokusK <55874907+KrokusK@users.noreply.github.com>
2020-03-06 21:56:07 +0600

    Update Continent.php

--- frontend/models/Continent.php
+++ frontend/models/Continent.php
@@ -86,12 +86,12 @@ class Continent extends \yii\db\ActiveRecord
     {
         // Search data
         $queryContinent = Continent::find()
-            //->leftJoin('country','country.continent_id = continent.id');
-            ->leftJoin('city','city.country_id = country.id');
+            ->innerJoin('country','country.continent_id = continent.id')
+            ->innerJoin('city','city.country_id = country.id');
         // Add data filter
-        //$this->setContinentFilter($queryContinent, $params);
-        //$this->setCountryFilter($queryContinent, $params);
-        //$this->setCityFilter($queryContinent, $params);
+        $this->setContinentFilter($queryContinent, $params);
+        $this->setCountryFilter($queryContinent, $params);
+        $this->setCityFilter($queryContinent, $params);
         // Add pagination params
         $this->setPaginationParams($queryContinent, $params);
         // get data

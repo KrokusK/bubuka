3224c8b27c4bb4a093a61222ee603fae0e4d9847
KrokusK <55874907+KrokusK@users.noreply.github.com>
2020-03-07 15:39:02 +0600

    Update Continent.php

--- frontend/models/Continent.php
+++ frontend/models/Continent.php
@@ -94,7 +94,8 @@ class Continent extends \yii\db\ActiveRecord
             //->select(['country.*'])
             ->leftJoin('country','country.continent_id = continent.id')
             ->leftJoin('city','city.country_id = country.id')
-            ->groupBy(['continent.id', 'country.name', 'city.name', 'city.population'])
+            //->groupBy(['continent.id', 'country.name', 'city.name', 'city.population'])
+            ->groupBy(['continent.id', 'city.population'])
             ->orderBy(['city.population' => SORT_ASC]);
             //->orderBy(['continent.name' => SORT_ASC, 'country.name' => SORT_ASC, 'city.name' => SORT_ASC]);
         // Add data filter

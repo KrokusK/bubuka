c0b9341a98a6678f30f334b15399cfb16372c194
KrokusK <55874907+KrokusK@users.noreply.github.com>
2020-03-10 10:38:45 +0600

    Update Continent.php

--- frontend/models/Continent.php
+++ frontend/models/Continent.php
@@ -106,7 +106,7 @@ class Continent extends \yii\db\ActiveRecord
             ->leftJoin('city','city.country_id = country.id')
             //->groupBy(['continent.id', 'country.id', 'city.id'])
             //->groupBy(['continent.id', 'city.population'])
-            ->orderBy(['city.population' => SORT_DESC]);
+            //->orderBy(['city.population' => SORT_DESC]);
             //->orderBy(['city.population' => SORT_ASC]);
             //->orderBy(['continent.name' => SORT_ASC, 'country.name' => SORT_ASC, 'city.name' => SORT_ASC]);
         // Add data filter
@@ -301,10 +301,10 @@ class Continent extends \yii\db\ActiveRecord
                 if (array_key_exists($params[$value], $orderTrend)) {
                     $orderValue = $orderTrend[$params[$value]];
                 }
-                //if (!empty($orderKey) && !empty($orderValue)) {
-                //    $query->orderBy([$orderKey => $orderValue]);
-                //}
             }
+        }
+        if (!empty($orderKey) && !empty($orderValue)) {
+            $query->orderBy([$orderKey => $orderValue]);
         }
 
         //return $orderKey." - ".$orderValue;
